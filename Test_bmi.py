998ba2349bee66760320eb46f53bc036e5ab9d90
Sheik <smssheik20.24@ichat.sp.edu.sg>
2025-05-08 16:32:42 +0800

    Added BMI unit tests

---
+++ Test_bmi.py
@@ -0,0 +1,12 @@
+import bmi.py as bmi
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.8, 50) 
+    assert result[1] == -1
+
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.7, 60)  
+    assert result[1] == 0
+
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.6, 80)  
+    assert result[1] == 1
